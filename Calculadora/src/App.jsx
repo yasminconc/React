4b08ccd33c23c7ae12272451f9ba88d357cf321e
Yasmin Silva da Conceição <yasmisilvaconc@gmail.com>
2026-02-04 18:48:12 -0300

    button

--- Calculadora/src/App.jsx
+++ Calculadora/src/App.jsx
@@ -1,10 +1,10 @@
+import Button from './components/button'
 import Text from './components/text'
 
 const App = () => {
   return (
     <div class='py-28 px-4'>
-      <Text as='h1' variant='heading'>Calculadora</Text>
-      <Text variant='muted'>Valor secundário</Text>
+    
     </div>
   )
 }
